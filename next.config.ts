e7e8d6b7d58f0b3bfb594f1b03dabcadf926076d
bonheurfungazer <109146663+bonheurfungazer@users.noreply.github.com>
2025-05-01 00:10:17 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output : "true",
   eslint: {
     // Avertissement : cela permet à la construction de production de réussir même si votre projet a des erreurs ESLint.
     ignoreDuringBuilds: true,
